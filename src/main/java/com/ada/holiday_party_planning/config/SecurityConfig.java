c8af3bd35de6d67bc6059eabd6ffe02c2963ae62
Vanessa Rutkoski <nessartk@gmail.com>
2024-11-10 19:14:45 -0300

    Mergeando com a main

--- src/main/java/com/ada/holiday_party_planning/config/SecurityConfig.java
+++ src/main/java/com/ada/holiday_party_planning/config/SecurityConfig.java
@@ -5,28 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * Configuração de segurança da aplicação usando Spring Security.
- * Esta configuração permite o acesso irrestrito a todos os endpoints
- * e usa a autenticação HTTP Basic.
- */
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    /**
-     * Configura o filtro de segurança para permitir que todas as solicitações
-     * sejam acessíveis sem autenticação. Usa HTTP Basic para autenticação por padrão,
-     * mas, como todas as solicitações são permitidas, qualquer usuário pode acessar qualquer endpoint.
-     *
-     * @param http o objeto HttpSecurity que configura a segurança da aplicação
-     * @return o filtro de segurança configurado (SecurityFilterChain)
-     * @throws Exception em caso de erro na configuração de segurança
-     */
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
